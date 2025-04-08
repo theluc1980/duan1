d872cb8e6ce37665375ac4ee9ad34a00e89cd2d1
theluc1980 <73338156+theluc1980@users.noreply.github.com>
2025-04-08 11:30:22 +0700

    Update app.py
    điều chỉnh lại

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello Heroku!"
+    return "Xin chào! Đây là ứng dụng Flask chạy trên Heroku!"
 
-if __name__ == '__main__':
-    app.run(debug=False)  # Luôn đặt debug=False khi chạy production
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
