a8fb8c4257b9a6e639d7060c82b41778f0f1152a
theluc1980 <73338156+theluc1980@users.noreply.github.com>
2025-04-08 11:15:07 +0700

    v0.1
    phiên bản thử

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello Heroku!"
+
+if __name__ == '__main__':
+    app.run(debug=False)  # Luôn đặt debug=False khi chạy production
