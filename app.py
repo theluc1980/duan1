9e7e2678f67140e3955519dfe6c3928b2be016de
theluc1980 <73338156+theluc1980@users.noreply.github.com>
2025-04-08 14:50:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Xin chào! Đây là ứng dụng Flask chạy trên Heroku!"
+    return "Hello Heroku!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)  # Quan trọng!
