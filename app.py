588a17ae9be83488ccc1de9142224481b09c9a58
theluc1980 <73338156+theluc1980@users.noreply.github.com>
2025-04-17 10:44:23 +0700

    Update app.py
    v7

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Xin chào! Đây là ứng dụng Flask chạy trên Heroku! thank you to you "
-    return "Cuộc sống tốt đẹp hơn khi ta thành công "
+    return "Xin chào! Đây là ứng dụng Flask chạy trên Heroku! thank you to you Cuộc sống tốt đẹp hơn khi ta thành công"
+ 
 if __name__ == '__main__':
     app.run()
